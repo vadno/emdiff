11f8d0b36dedc3c735997d845ba5eb8b670ca17f
vadno <vadasznoemi@gmail.com>
2020-01-11 09:35:45 +0100

    fuggosegi eval hozzaadva

--- eval.py
+++ eval.py
@@ -1,6 +1,6 @@
 import nltk.chunk
 import nltk.parse.evaluate
-
+import nltk.chunk.util
 
 def diff_tags(delta, column):
     """
@@ -20,6 +20,8 @@ def diff_tags(delta, column):
         if line[0] != '+' and line[1] != '-':
             if line[0][column[0]] == line[1][column[1]]:
                 tp += 1
+            elif line[0][column[0]] != line[1][column[1]]:
+                pass
             total += 1
 
     print('accuracy: {0:.2%}'.format(tp/total))
@@ -53,7 +55,32 @@ def diff_chunks(delta, column):
     for line in delta:
         if line[0] != '+' and line[1] != '-':
             if line[0][column[0]] == line[1][column[1]]:
-                pass
+                print(line[0][column[0]])
+
+
+def process_sentence(sent, head, deprel):
+
+    # Percentage of words that get the correct head and label
+    # LAS
+    # Percentage of words that get the correct head
+    # UAS
+
+    corr = 0
+    corrl = 0
+    total = 0
+
+    # TODO 0 hosszú mondatokra (mondatzáró)
+    # TODO kitesztelni a függőségit!
+
+    for line in sent:
+        total += 1
+        if line[0][head[0]] == line[1][head[1]]:
+            corr += 1
+            if line[0][deprel[0]] == line[1][deprel[1]]:
+                corrl += 1
+
+    print('LAS: ', str(corrl/total))
+    print('UAS: ', str(corr/total))
 
 
 def diff_deps(delta, column, head, deprel):
@@ -65,9 +92,30 @@ def diff_deps(delta, column, head, deprel):
     :param deprel:
     :return:
     """
-
+    #
     # for line in delta:
     #     print(line[0][column[0]], line[1][column[1]])
     #     print(line[0][head[0]], line[1][head[1]])
     #     print(line[0][deprel[0]], line[1][deprel[1]])
 
+    sent = list()
+    tokenerror = False
+
+    for line in delta:
+
+        if 'newsent' not in line[0]:
+
+            # TODO kivenni a nem egyformán tokenizált mondatokat
+            if line[0] == '+' or line[1] == '-':
+                tokenerror = True
+
+            if line[0] != '+' and line[1] != '-':
+                sent.append(line)
+
+        elif 'newsent' in line[0]:
+            process_sentence(sent, head, deprel)
+            tokenerror = False
+            sent = list()
+
+    if sent:
+        process_sentence(sent, head, deprel)
