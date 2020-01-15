931511ce783454103b6b703fc49502b41502fdea
vadasz <vadasznoemi@gmail.com>
2020-01-15 12:08:11 +0100

    dependency agreement hozzaadva

--- agree.py
+++ agree.py
@@ -1,31 +1,7 @@
 from nltk.metrics.agreement import AnnotationTask   # https://www.nltk.org/_modules/nltk/metrics/agreement.html
 
 
-def coeff(annot_data):
-    """
-    az nltk.metrics.agreement-ben elérhető egyetértési metrikákkal számol
-    :param annot_data: a megfelelő formátumban megkapott adat, ami:
-    az annotálandó elemek listája, ahol az egyes elemek
-    háromelemű listák, ahol
-        az első elem az annotátor azonosítója
-        a második elem a szóalak
-        a harmadik elem a címke
-    :return:
-    """
-    # print(annot_data)
-
-    task = AnnotationTask(data=annot_data)
-    print('percentage of agreement, observed agreement')
-    print(task.avg_Ao())
-
-    print('chance-corrected coefficients')
-    print('Bennett, Albert and Goldstein S (1954)\n(all categories are equally likely): ', task.S())
-    print('Scott pi (1955)\n(single distribution): ', task.pi())
-    print('Cohen kappa (1960)\n(individual coder distribution): ', task.kappa())
-    print('weighted kappa: ', task.weighted_kappa())
-
-
-def reverse_data(delta, column):
+def reverse_tags(delta, column):
     """
     a megfelelő formátumra hozza az annotált adatot, aminek specifikációja a coeff() függvényben van
     :param delta: az összevetett adat
@@ -35,11 +11,56 @@ def reverse_data(delta, column):
 
     by_tokens = list()
     for line in delta:
-        if line[0] != '+' and line[1] != '-':
+        if line[0] != '+' and line[1] != '-' and 'newsent' not in line[0]:
             by_tokens.append(['g', line[0][0], line[0][column[0]]])
             by_tokens.append(['s', line[0][0], line[1][column[1]]])
 
     return by_tokens
+
+
+def reverse_deps(delta, head, deprel):
+    """
+
+    :param delta:
+    :param head:
+    :param deprel:
+    :return:
+    """
+
+    by_deps = list()
+    for line in delta:
+        if line[0] != '+' and line[1] != '-' and 'newsent' not in line[0]:
+            by_deps.append(((line[0][head[0]], line[0][deprel[0]]), (line[1][head[1]], line[1][deprel[1]])))
+
+    return by_deps
+
+
+def agree_dep(delta, head, deprel):
+    """
+
+    :param delta:
+    :param head:
+    :param deprel:
+    :return:
+    """
+
+    by_dep = reverse_deps(delta, head, deprel)
+
+    ua = 0  # unlabeled attachment
+    la = 0  # labeled attachment
+    lo = 0  # label only
+    total = 0
+
+    for dep in by_dep:
+        total += 1
+        if dep[0] == dep[1]:
+            la += 1
+        if dep[0][0] == dep[1][0]:
+            ua += 1
+        if dep[0][1] == dep[1][1]:
+            lo += 1
+
+    return ua / total, la / total, lo / total
 
 
 def agree_tags(delta, column):
@@ -49,5 +70,15 @@ def agree_tags(delta, column):
     :param column:  az az oszlop, amelyre egyetértést akarunk számolni
     :return:
     """
-    by_field = reverse_data(delta, column)
-    coeff(by_field)
+    by_field = reverse_tags(delta, column)
+
+    task = AnnotationTask(data=by_field )
+
+    oa = task.avg_Ao()      # observed agreement
+    s = task.S()            # Bennett, Albert and Goldstein S (1954) all categories are equally likely
+    pi = task.pi()          # Scott pi (1955) single distribution
+    kappa = task.kappa()    # Cohen kappa (1960) individual coder distribution
+    w_kappa = task.weighted_kappa()
+    alpha = task.alpha()    # Krippendorff alpha (1980)
+
+    return oa, s, pi, kappa, w_kappa, alpha
